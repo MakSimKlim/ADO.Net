96a10caba642461c407866240bf65e7bfacecd49
Klimov_Maxim <120740542+MakSimKlim@users.noreply.github.com>
2024-04-07 22:18:04 +0500

    Add files via upload

--- ДЗ №80 до 04.04.24 ADO_Net_ Особ_Entity Framework(WPF)/MainWindow.xaml.cs
+++ ДЗ №80 до 04.04.24 ADO_Net_ Особ_Entity Framework(WPF)/MainWindow.xaml.cs
@@ -15,125 +15,295 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 using Microsoft.Win32; // Добавлено для извлечения картинок из базы данных
 using System.IO; // Добавлено для извлечения картинок из базы данных
+using System.Data.SqlClient;
+using System.Data.Entity.Infrastructure;
+
 
 namespace BooksLibrary
 {
     public partial class MainWindow : Window
     {
-        private List<Book> books;
-        private int currentBookIndex;
+        readonly BooksLibraryEntities _context;
+        private List<Book> _books;
+        private int _currentBookIndex;
 
         public MainWindow()
         {
             InitializeComponent();
-            LoadBooksFromDatabase();
+            LoadStatuses(); //  получить список статусов из базы данных
+            _context = new BooksLibraryEntities();
+            _books = _context.Books.Include("Author").Include("Status").ToList();
+            _currentBookIndex = 0;
             DisplayCurrentBook();
         }
 
-        private void LoadBooksFromDatabase()
-        {
-            // Загрузите все книги из вашей базы данных в список "books"
-            // Это будет зависеть от того, как вы настроили свою базу данных и Entity Framework
-            // Например: books = dbContext.Books.Include(b => b.Author).Include(b => b.Status).ToList();
-        }
-
+        // загрузка текущей книги
         private void DisplayCurrentBook()
         {
-            // Отобразите информацию о текущей книге в текстовых полях
-            //var book = books[currentBookIndex];
-            //AuthorTextBox.Text = book.Author.Name;
-            //TitleTextBox.Text = book.Title;
-            //PageReadTextBox.Text = book.PagesRead.ToString();
-            //PageTextBox.Text = book.TotalPages.ToString();
-            //StatusTextBox.Text = book.Status.Title;
-            //RatingTextBox.Text = book.Rating.ToString();
-
-            // Загрузите изображение обложки из базы данных
-            // Это будет зависеть от того, как вы храните изображения в вашей базы данных
-            // Например: CoverImage.Source = new BitmapImage(new Uri(book.Cover));
+            var book = _books[_currentBookIndex];
+            if (book != null && book.Author != null)
+            {
+                AuthorTextBox.Text = book.Author.AuthorName + " " + book.Author.AuthorSurname;
+            }
+            else
+            {
+                // Обработка ситуации, когда book или book.Author равны null
+                AuthorTextBox.Text = string.Empty;
+            }            
+            TitleTextBox.Text = book.BookTitle;
+            PageReadTextBox.Text = book.NumberOfPagesRead.ToString();
+            PageTextBox.Text = book.NumberOfPages.ToString();
+            StatusComboBox.Text = book.Status.TitleStatus;
+            RatingTextBox.Text = book.Rating.ToString();
+            CoverImage.Source = LoadImage(book.Cover); // Загрузка изображения из массива байтов
         }
-
+        // загрузка изображения
+        private BitmapImage LoadImage(byte[] imageData)
+        {
+            if (imageData == null || imageData.Length == 0) return null;
+            var image = new BitmapImage();
+            using (var mem = new MemoryStream(imageData))
+            {
+                mem.Position = 0;
+                image.BeginInit();
+                image.CreateOptions = BitmapCreateOptions.PreservePixelFormat;
+                image.CacheOption = BitmapCacheOption.OnLoad;
+                image.UriSource = null;
+                image.StreamSource = mem;
+                image.EndInit();
+            }
+            image.Freeze();
+            return image;
+        }
+        // переход к предыдущей книге
         private void PreviousButton_Click(object sender, RoutedEventArgs e)
         {
-            if (currentBookIndex > 0)
+            if (_currentBookIndex > 0)
             {
-                currentBookIndex--;
+                _currentBookIndex--;
                 DisplayCurrentBook();
             }
         }
-
+        // переход к следующей книге
         private void NextButton_Click(object sender, RoutedEventArgs e)
         {
-            if (currentBookIndex < books.Count - 1)
+            if (_currentBookIndex < _books.Count - 1)
             {
-                currentBookIndex++;
+                _currentBookIndex++;
                 DisplayCurrentBook();
             }
         }
 
+        // загрузка статусов в combobox
+        private void LoadStatuses()
+        {
+            // выполнение запроса к базе данных
+            // и получения списка статусов
+            List<string> statuses = GetStatusesFromDatabase();
+
+            // Заполняем StatusComboBox полученными статусами
+            foreach (var status in statuses)
+            {
+                StatusComboBox.Items.Add(status);
+            }
+        }
+        private List<string> GetStatusesFromDatabase()
+        {
+            List<string> statuses = new List<string>();
+
+            // Создаем подключение к базе данных
+            // Подключение к базе данных закрывается автоматически при выходе из блока using
+            using (SqlConnection connection = new SqlConnection("Data Source=(local);Initial Catalog=BooksLibrary;Integrated Security=True"))
+            {
+                // Открываем подключение
+                connection.Open();
+
+                // Создаем команду SQL
+                using (SqlCommand command = new SqlCommand("SELECT TitleStatus FROM Statuses", connection))
+                {
+                    // Выполняем команду и получаем результаты
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        // Читаем результаты
+                        while (reader.Read())
+                        {
+                            statuses.Add(reader.GetString(0));
+                        }
+                    }
+                }
+            }
+
+            return statuses;
+        }
+        // при выборе статуса просто отображается список статусов
+        private void StatusComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            // Ничего не делаем при выборе статуса
+        }
+
+        // кнопка сохранить в базу данных с проверкой на триггеры
         private void SaveButton_Click(object sender, RoutedEventArgs e)
         {
-            // Создайте новую книгу с информацией из текстовых полей и добавьте ее в базу данных
-            // Это будет зависеть от того, как вы настроили свою базу данных и Entity Framework
-            // Например:
-            // var newBook = new Book { ... };
-            // dbContext.Books.Add(newBook);
-            // dbContext.SaveChanges();
+            try
+            {
+                var newBook = new Book
+                {
+                    BookTitle = TitleTextBox.Text,
+                    Author = _context.Authors.FirstOrDefault(a => a.AuthorName == AuthorTextBox.Text), // Предполагается, что имя автора уникально
+                    NumberOfPagesRead = int.Parse(PageReadTextBox.Text),
+                    NumberOfPages = int.Parse(PageTextBox.Text),
+                    Status = _context.Statuses.FirstOrDefault(s => s.TitleStatus == StatusComboBox.Text), // Предполагается, что статус уникален
+                    Rating = int.Parse(RatingTextBox.Text),
+                    //Cover = ImageToByteArray(CoverImage.Source as BitmapImage) // Преобразование изображения в массив байтов
+                };
+                _context.Books.Add(newBook);
+                _context.SaveChanges();
+                _books = _context.Books.Include("Author").Include("Status").ToList();// Обновление списка книг
+
+            }
+            catch (DbUpdateException ex)
+            {
+                // Проверяем внутреннее исключение
+                var innerException = ex.InnerException;
+                while (innerException != null)
+                {
+                    // Если внутреннее исключение является SqlException, обрабатываем его
+                    if (innerException is SqlException sqlEx)
+                    {
+                        // Проверяем номер ошибки
+                        if (sqlEx.Number == 50000) // номер ошибки для пользовательских ошибок RAISERROR
+                        {
+                            // Отображаем сообщение об ошибке
+                            MessageBox.Show(sqlEx.Message, "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+                        }
+                        else
+                        {
+                            // Обрабатываем все остальные ошибки SQL
+                            MessageBox.Show("Произошла ошибка при работе с базой данных: " + sqlEx.Message, "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+                        }
+                    }
+
+                    // Переходим к следующему внутреннему исключению
+                    innerException = innerException.InnerException;
+                }
+            }
         }
 
+        // кнопка откорректировать книгу в базе данных с проверкой на триггеры
         private void CorrectButton_Click(object sender, RoutedEventArgs e)
         {
-            // Обновите информацию о текущей книге в базе данных с информацией из текстовых полей
-            // Это будет зависеть от того, как вы настроили свою базу данных и Entity Framework
-            // Например:
-            // var book = books[currentBookIndex];
-            // book.Title = TitleTextBox.Text;
-            // ...
-            // dbContext.SaveChanges();
-        }
-
-        //private void DownloadCoverButton_Click(object sender, RoutedEventArgs e)
-        //{
-        //    // Сохраните изображение обложки текущей книги
-        //    // Это будет зависеть от того, как вы храните изображения в вашей базы данных
-        //    // Например:
-        //    // SaveFileDialog saveFileDialog = new SaveFileDialog();
-        //    // saveFileDialog.Filter = "Images (*.BMP;*.JPG;*.PNG)|*.BMP;*.JPG;*.PNG";
-        //    // if (saveFileDialog.ShowDialog() == true)
-        //    // {
-        //    //     File.Copy(book.Cover, saveFileDialog.FileName);
-        //    // }
-        //}
-
-        private void WallImage_MouseDown(object sender, MouseButtonEventArgs e)
-        {
-            // Создайте новое окно
-            Window imageWindow = new Window
+            try
             {
-                Width = CoverImage.Width * 5,
-                Height = CoverImage.Height * 5,
-                Content = new Image { Source = CoverImage.Source }
-            };
+                var book = _books[_currentBookIndex];
+                book.BookTitle = TitleTextBox.Text;
+                book.Author = _context.Authors.FirstOrDefault(a => a.AuthorName == AuthorTextBox.Text); // Предполагается, что имя автора уникально
+                book.NumberOfPagesRead = int.Parse(PageReadTextBox.Text);
+                book.NumberOfPages = int.Parse(PageTextBox.Text);
+                book.Status = _context.Statuses.FirstOrDefault(s => s.TitleStatus == StatusComboBox.Text); // Предполагается, что статус уникален
+                book.Rating = int.Parse(RatingTextBox.Text);
+                //book.Cover = ImageToByteArray(CoverImage.Source as BitmapImage); // Преобразование изображения в массив байтов
+                _context.SaveChanges();
+                _books = _context.Books.Include("Author").Include("Status").ToList();// Обновление списка книг
+            }
+            catch (DbUpdateException ex)
+            {
+                // Проверяем внутреннее исключение
+                var innerException = ex.InnerException;
+                while (innerException != null)
+                {
+                    // Если внутреннее исключение является SqlException, обрабатываем его
+                    if (innerException is SqlException sqlEx)
+                    {
+                        // Проверяем номер ошибки
+                        if (sqlEx.Number == 50000) // номер ошибки для пользовательских ошибок RAISERROR
+                        {
+                            // Отображаем сообщение об ошибке
+                            MessageBox.Show(sqlEx.Message, "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+                        }
+                        else
+                        {
+                            // Обрабатываем все остальные ошибки SQL
+                            MessageBox.Show("Произошла ошибка при работе с базой данных: " + sqlEx.Message, "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+                        }
+                    }
 
-            // Отобразите окно
-            imageWindow.Show();
+                    // Переходим к следующему внутреннему исключению
+                    innerException = innerException.InnerException;
+                }
+            }
         }
 
+        // кнопка сохранить обложку в нужном формате
         private void DownloadCoverButton_Click(object sender, RoutedEventArgs e)
         {
-            SaveFileDialog saveFileDialog = new SaveFileDialog();
-            saveFileDialog.Filter = "Images (*.BMP;*.JPG;*.PNG)|*.BMP;*.JPG;*.PNG";
-            if (saveFileDialog.ShowDialog() == true)
+            // Получаем выбранную книгу
+            var selectedBook = _books[_currentBookIndex];
+            if (selectedBook != null)
             {
-                var encoder = new PngBitmapEncoder();
-                encoder.Frames.Add(BitmapFrame.Create((BitmapSource)CoverImage.Source));
-                using (FileStream stream = new FileStream(saveFileDialog.FileName, FileMode.Create))
+                // Столбец Cover в таблице Books хранит обложки книг в формате byte[]
+                byte[] imageBytes = selectedBook.Cover;
+                if (imageBytes != null)
                 {
-                    encoder.Save(stream);
+                    // Создаем диалог для сохранения файла
+                    var saveFileDialog = new SaveFileDialog();
+                    saveFileDialog.Filter = "JPEG Image|*.jpg|Bitmap Image|*.bmp|PNG Image|*.png"; // определение фильтров для типа файлов
+                    if (saveFileDialog.ShowDialog() == true)
+                    {
+                        // Сохраняем изображение в выбранный файл
+                        File.WriteAllBytes(saveFileDialog.FileName, imageBytes);
+                    }
                 }
             }
         }
 
 
+        // Обработчик события клика мышью на изображении (увеличивает изображение в отдельном окне)
+        private void CoverImage_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            // Создаем новое окно
+            Window imageWindow = new Window
+            {
+                Title = "Увеличенное изображение",
+                Width = 800,
+                Height = 800
+            };
+
+            // Создаем новый Image контрол и устанавливаем его как контент окна
+            Image largeImage = new Image
+            {
+                Source = CoverImage.Source,
+                Stretch = Stretch.Uniform
+            };
+            imageWindow.Content = largeImage;
+
+            // Открываем окно
+            imageWindow.Show();
+        }
+
+        // кнопка очистки всех полей
+        private void ClearButton_Click(object sender, RoutedEventArgs e)
+        {
+            AuthorTextBox.Text = string.Empty;
+            TitleTextBox.Text = string.Empty;
+            PageReadTextBox.Text = string.Empty;
+            PageTextBox.Text = string.Empty;
+            StatusComboBox.Text = string.Empty;
+            RatingTextBox.Text = string.Empty;
+            CoverImage.Source = null; // Очистка изображения обложки
+        }
+
+
+        //private byte[] ImageToByteArray(BitmapImage bitmapImage)
+        //{
+        //    byte[] data;
+        //    var encoder = new PngBitmapEncoder();
+        //    encoder.Frames.Add(BitmapFrame.Create(bitmapImage));
+        //    using (var ms = new MemoryStream())
+        //    {
+        //        encoder.Save(ms);
+        //        data = ms.ToArray();
+        //    }
+        //    return data;
+        //}
     }
 }
